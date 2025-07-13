9a2ac7fd6b51f44fdb7e0faee0f93cd1e2cf1285
ln-vain <youinvain@gmail.com>
2025-07-13 18:58:59 +0500

    Add Number Guessing Game project

---
+++ Number_Guessing_Game/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number_Guessing_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            int lower, higher;
+            int triescount = 3;
+            int userInput;
+
+            Random rand = new Random();
+
+            number = rand.Next(0, 201);
+
+            lower = number - 10;
+            if (lower < 0) lower = 0;
+            lower = rand.Next(lower, number);
+
+            higher = number + 10;
+            if (higher > 100) higher = 100;
+            higher = rand.Next(number + 1, higher);
+
+            Console.WriteLine($"We quessed a number between 0 and 200. It's greater than {lower} and less than {higher}.");
+            Console.WriteLine($"What is this number? You have 3 attemps.");
+            Console.WriteLine();
+            while (triescount-- > 0)
+            {
+                Console.Write("Your answer is: ");
+                userInput = Convert.ToInt32(Console.ReadLine());
+
+
+                if (userInput == number)
+                {
+                    Console.WriteLine("You guessed the number!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("It's wrong number!");
+
+                    if (userInput > number && userInput - number <= 5)
+                    {
+                        Console.WriteLine("But so close. Just a bit lower.");
+                    }
+                    else if (userInput < number && number - userInput <= 5)
+                    {
+                        Console.WriteLine("But so close. Just a bit higher.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Wrong guess.");
+                        Console.WriteLine();
+                    }
+
+                    Console.WriteLine($"You have \"{triescount}\" attempts left.");
+                    Console.WriteLine();
+                }
+
+                if (triescount <= 0)
+                {
+                    Console.WriteLine($"You lost, but hope you'll be more lucky next time! The number was: \"{number}\"");
+                }
+            }
+        }
+    }
+}
---
+++ Number_Guessing_Game/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Number_Guessing_Game")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Number_Guessing_Game")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("2ebf80f5-ed15-4bcb-b231-6186402976b2")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
